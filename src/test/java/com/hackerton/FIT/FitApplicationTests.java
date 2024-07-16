ee4bf1ad17cbbcc770c7846777c28ff0ddf005c2
김가영 <yeong.kmj@gmail.com>
2024-07-16 16:15:03 +0900

    feat: initialize

---
+++ src/test/java/com/hackerton/FIT/FitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackerton.FIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
