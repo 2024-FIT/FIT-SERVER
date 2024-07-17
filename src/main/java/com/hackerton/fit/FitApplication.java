300e9b6e4d8cd9c117a6324af1d450da80f5f6ac
김가영 <yeong.kmj@gmail.com>
2024-07-17 11:12:05 +0900

    feat: entity

--- src/main/java/com/hackerton/fit/FitApplication.java
+++ src/main/java/com/hackerton/fit/FitApplication.java
@@ -2,8 +2,11 @@ package com.hackerton.fit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FitApplication {
 
 	public static void main(String[] args) {
