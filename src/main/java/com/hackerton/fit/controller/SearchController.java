ef6648961d0a7b0e0641ab213f4142343aa5d481
daedyu <134924951+daedyu@users.noreply.github.com>
2024-07-17 19:17:26 +0900

    feat: foodData

--- src/main/java/com/hackerton/fit/controller/SearchController.java
+++ src/main/java/com/hackerton/fit/controller/SearchController.java
@@ -1,15 +1,22 @@
 package com.hackerton.fit.controller;
 
+import com.hackerton.fit.dto.FoodDataDto;
+import com.hackerton.fit.entity.FoodDataEntity;
+import com.hackerton.fit.service.MealService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class SearchController {
-    @GetMapping("")
-    public void index(@RequestParam String food) {
+    private final MealService mealService;
 
+    @GetMapping("/search")
+    public List<FoodDataDto> index(@RequestParam String food) {
+         return mealService.getData(food);
     }
 }
