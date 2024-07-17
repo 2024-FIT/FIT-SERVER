bcc1b796664d491fa716e3e1ad069c9e5f67744e
daedyu <134924951+daedyu@users.noreply.github.com>
2024-07-18 02:11:02 +0900

    feat: calender

--- src/main/java/com/hackerton/fit/domain/mealTime/dto/MealTimeReq.java
+++ src/main/java/com/hackerton/fit/domain/mealTime/dto/MealTimeReq.java
@@ -4,13 +4,12 @@ import com.hackerton.fit.domain.meal.dto.NutritionDataDTO;
 import com.hackerton.fit.domain.meal.entity.MealEntity;
 import com.hackerton.fit.domain.mealTime.entity.MealTimeEntity;
 import com.hackerton.fit.domain.mealTime.service.enums.MealTimeEnums;
-import com.hackerton.fit.domain.user.entity.UserEntity;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
-public class MealTimeResponse {
+public class MealTimeReq {
     private LocalDate time;
 
     private MealTimeEnums mealTimeEnum;
